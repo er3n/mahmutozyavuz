6e17aa71c5f0dc8ac591ac6ec0975157bc3c465f
er3n <eren.ozturk@obss.com.tr>
2019-05-30 23:11:25 +0300

    asd

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use(genericRouteInterceptor);
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
